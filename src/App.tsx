be5ef8c2bae756685dbab840a659653f16317690
horiy0125 <k.horiy0125@gmail.com>
2022-07-16 03:53:56 +0900

    feat: add KiteIndexTemplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { KiteIndexTemplate } from './components/templates/KiteIndexTemplate';
+
 export const App: React.FC = () => {
   return (
     <div>
       <h1>Kite</h1>
+
+      <KiteIndexTemplate />
     </div>
   );
 };
---
+++ src/components/templates/KiteIndexTemplate.tsx
@@ -0,0 +1,7 @@
+export const KiteIndexTemplate: React.FC = () => {
+  return (
+    <div>
+      <h1>IndexPage</h1>
+    </div>
+  );
+};
